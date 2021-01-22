8612ebfb4ce82d88cedfa99c901c00d1c808b1ae
Raúl <raulguzmanguerrero@outlook.com>
2021-01-22 14:34:08 -0600

    Add server_data template

---
+++ server_data_template.js
@@ -0,0 +1,12 @@
+/* As this is just a project to test sockets the database is really basic. For the love that everything that's 
+holy don't use store production data here (especially passwords) */
+
+/* To use this file rename it to server_data.js */
+
+module.exports = {
+    users: {
+    },
+    active_users: {
+    },
+    
+}
