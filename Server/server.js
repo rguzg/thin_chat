2a5f1f1a2ddec32024682c63735f6489b4a1b96e
Raúl <raulguzmanguerrero@outlook.com>
2021-01-22 23:52:30 -0600

    Add socket pause and resume when reading data

--- Server/server.js
+++ Server/server.js
@@ -6,12 +6,14 @@ dotenv.config();
 
 // Create new TCP server and start listening
 const chat_server = net.createServer((client_socket) => {
-    console.log(`Recieved message from ${client_socket.remoteAddress}`);
+    console.log(`Connected to ${client_socket.remoteAddress}`);
     client_socket.on('data', (buffer) => {
+        client_socket.pause();
         // The response depends on the message in the socket
         let response = GenerateResponse(buffer);
 
         client_socket.write(response);
+        client_socket.resume();
     })
     
     client_socket.on("end", (hadError) => {
